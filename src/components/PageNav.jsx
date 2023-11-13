8a4ffa4aa898090c48cc1b7fac34a69db89cdebe
Awointa <akosawointa@gmail.com>
2023-11-13 14:10:15 +0100

    added some more styled components and added route to point the logo to the homePage

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import styles from "./PageNav.module.css";
 
 export default function PageNav() {
 	return (
-		<nav>
+		<nav className={styles.nav}>
+			<Logo />
 			<ul>
-				<li>
-					<NavLink to="/">HomePage</NavLink>
-				</li>
 				<li>
 					<NavLink to="/pricing">Pricing</NavLink>
 				</li>
 				<li>
 					<NavLink to="/product">product</NavLink>
+				</li>
+				<li>
+					<NavLink to="/login">Login</NavLink>
 				</li>
 			</ul>
 		</nav>
