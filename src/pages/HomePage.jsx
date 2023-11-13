41b49a6de9b848516075766dc0dfeab725c15b7f
Awointa <akosawointa@gmail.com>
2023-11-13 11:18:24 +0100

    Imported and displayed the NavLink component in each page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 
 export default function HomePage() {
 	return (
 		<div>
 			{" "}
+			<PageNav />
 			<h1>HomePage</h1>
 			<Link to="/pricing">pricing</Link>
 		</div>
