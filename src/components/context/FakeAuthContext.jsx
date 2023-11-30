3352749a9a7e0d759c88eb07ac244842efb79170
Awointa <akosawointa@gmail.com>
2023-11-30 15:00:40 +0100

    corrected spelling error in the Fake_User email property

--- src/components/context/FakeAuthContext.jsx
+++ src/components/context/FakeAuthContext.jsx
@@ -1,4 +1,4 @@
-import { useContext, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
@@ -7,36 +7,32 @@ const initialState = {
 	isAuthenticated: false,
 };
 
-const FAKE_USER = {
-	name: "Jack",
-	email: "jack@example.com",
-	password: "qwerty",
-	avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
-function reducer(action, state) {
+function reducer(state, action) {
 	switch (action.type) {
 		case "login":
-			return {
-				...state,
-				user: action.payload,
-				isAuthenticated: true,
-			};
+			return { ...state, user: action.payload, isAuthenticated: true };
 		case "logout":
 			return { ...state, user: null, isAuthenticated: false };
 		default:
-			throw new Error("Unknown action type");
+			throw new Error("Unknown action");
 	}
 }
 
+const FAKE_USER = {
+	name: "jack",
+	email: "jack@example.com",
+	password: "zambazi",
+	avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 function AuthProvider({ children }) {
-	const [{ user, isAuthenticated, login, logout }, dispatch] = useReducer(
+	const [{ user, isAuthenticated }, dispatch] = useReducer(
 		reducer,
 		initialState,
 	);
 
 	function login(email, password) {
-		if (email == FAKE_USER.emial && password == FAKE_USER.password)
+		if (email === FAKE_USER.email && password === FAKE_USER.password)
 			dispatch({ type: "login", payload: FAKE_USER });
 	}
 
@@ -53,8 +49,8 @@ function AuthProvider({ children }) {
 
 function useAuth() {
 	const context = useContext(AuthContext);
-	if (context == undefined)
-		throw new Error("AuthContext was used outside Authprovider");
+	if (context === undefined)
+		throw new Error("AuthContext was used outside AuthProvider");
 	return context;
 }
 
